51303f11df130051169654bbd6bb9f5a52526cc2
Fatih Kabay <mfthkabay@gmail.com>
2023-10-11 20:21:34 +0300

    added send message function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import io from 'socket.io-client';
+
+const socket = io.connect("http://localhost:3001")
 
 function App() {
+
+  const sendMessage = () => { 
+     //socket.emit();
+  }
+
   return (
-    <div className="container"></div>
+    <div className="container">
+      <input placeholder='Message...'/>
+      <button onClick={sendMessage}>Send Message</button>
+    </div>
   );
 }
 
