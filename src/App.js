3c7b6aa6ac93448817079e980841fe9f295ed5c8
Fatih Kabay <mfthkabay@gmail.com>
2023-10-14 23:52:45 +0300

    send message is successfully

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import io from 'socket.io-client';
+import { useEffect } from 'react';
 
 const socket = io.connect("http://localhost:3001")
 
 function App() {
 
   const sendMessage = () => { 
-     //socket.emit();
+     socket.emit("send_message", { message: "Hello!"});
   }
+  
+  useEffect(() => {
+     socket.on("receive_message", (data) => {
+       alert(data.message);
+     });
+  }, [socket])
 
   return (
     <div className="container">
